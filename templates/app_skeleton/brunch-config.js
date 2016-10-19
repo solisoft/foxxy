a9377b5572aa8b66593207c07c3bcab1655c4f28
Olivier BONNAURE <olivier@solisoft.net>
2016-10-19 16:30:59 +0200

    Brunch config / optimize CSS injection

--- templates/app_skeleton/brunch-config.js
+++ templates/app_skeleton/brunch-config.js
@@ -14,10 +14,18 @@ module.exports = {
   hooks: {
     preCompile: (end) => {
       var fs = require('fs');
+      // JS
       fs.createReadStream('node_modules/uikit/dist/js/components/pagination.js').pipe(fs.createWriteStream('app/vendors/uikit-pagination.js'));
       fs.createReadStream('node_modules/uikit/dist/js/components/form-select.js').pipe(fs.createWriteStream('app/vendors/uikit-form-select.js'));
       fs.createReadStream('node_modules/uikit/dist/js/components/datepicker.js').pipe(fs.createWriteStream('app/vendors/uikit-datepicker.js'));
-
+      fs.createReadStream('node_modules/uikit/dist/js/components/notify.js').pipe(fs.createWriteStream('app/vendors/uikit-notify.js'));
+      fs.createReadStream('node_modules/select2/dist/js/select2.js').pipe(fs.createWriteStream('app/vendors/select2.js'));
+      // CSS
+      fs.createReadStream('node_modules/uikit/dist/css/uikit.almost-flat.css').pipe(fs.createWriteStream('app/vendors/0-uikit.css'));
+      fs.createReadStream('node_modules/uikit/dist/css/components/datepicker.gradient.css').pipe(fs.createWriteStream('app/vendors/1-uikit-datepicker.css'));
+      fs.createReadStream('node_modules/uikit/dist/css/components/form-select.gradient.css').pipe(fs.createWriteStream('app/vendors/3-uikit-form-select.css'));
+      fs.createReadStream('node_modules/uikit/dist/css/components/notify.gradient.css').pipe(fs.createWriteStream('app/vendors/4-uikit-notify.css'));
+      // Fonts
       fs.createReadStream('node_modules/uikit/dist/fonts/fontawesome-webfont.ttf').pipe(fs.createWriteStream('app/assets/fonts/fontawesome-webfont.ttf'));
       fs.createReadStream('node_modules/uikit/dist/fonts/fontawesome-webfont.woff').pipe(fs.createWriteStream('app/assets/fonts/fontawesome-webfont.woff'));
       fs.createReadStream('node_modules/uikit/dist/fonts/fontawesome-webfont.woff2').pipe(fs.createWriteStream('app/assets/fonts/fontawesome-webfont.woff2'));

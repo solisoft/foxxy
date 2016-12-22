630a824de7602a127c6c5d325b3bc9d74222003f
Olivier BONNAURE <olivier@solisoft.net>
2016-12-22 10:51:14 +0100

    Fix email.js

--- templates/auth/foxx/widget/APP/scripts/send-mail.js
+++ templates/auth/foxx/widget/APP/scripts/send-mail.js
@@ -1,20 +1,20 @@
 'use strict';
 const db = require('@arangodb').db;
-const _settings = db._collection('_settings').firstExample();
+const settings = db._collection('foxxy_settings').firstExample();
 const request = require('@arangodb/request');
 const params = module.context.argv[0];
 console.log(`Sent to ${module.context.argv[0].to}`);
 
 request({
   method: "POST",
-  url: "https://api:"+ _settings.mailgun_apikey +"@api.mailgun.net/v3/"+ module.context.configuration.mailgun_domain +"/messages",
+  url: "https://api:"+ settings.mailgun_apikey +"@api.mailgun.net/v3/"+ module.context.configuration.mailgun_domain +"/messages",
   form: {
-    from: _settings.mailgun_from,
+    from: settings.mailgun_from,
     to: params.to,
     subject: "Email confirmation",
     text: `Hi,
 
-To confirm your email address, please clik on the link below :
+To confirm your email address, please click on the link below :
 https://${_settings.domain}/#confirm/${params.uuid}
 
 If you don't know about this email, please ignore it

06364aeba4882bb5da2e46995772c4fe67a74e13
Olivier BONNAURE <olivier@solisoft.net>
2017-02-12 11:54:55 +0100

    mailer fix for auth service

--- templates/auth/foxx/widget/APP/scripts/lost-password.js
+++ templates/auth/foxx/widget/APP/scripts/lost-password.js
@@ -1,6 +1,17 @@
 'use strict';
 const request = require('@arangodb/request');
 const params = module.context.argv[0];
+const settings = db._collection('foxxy_settings').firstExample();
+const email_text = `Hi,
+
+You asked to change your password. Please follow instruction on link below :
+http://${module.context.configuration.domain}/login#change_password/${params.uuid}
+
+Best,
+Foxxy App`
+
+console.log(`Sent to ${params.to}`);
+console.log(email_text);
 
 request({
   method: "POST",
@@ -9,13 +20,7 @@ request({
     from: settings.mailgun_from,
     to: params.to,
     subject: "Lost password",
-    text: `Hi,
-
-You asked to change your password. Please follow instruction on link below :
-http://${module.context.configuration.domain}/login#change_password/${params.uuid}
-
-Best,
-Foxxy App`
+    text: email_text
   }
 })
 
--- templates/auth/foxx/widget/APP/scripts/send-mail.js
+++ templates/auth/foxx/widget/APP/scripts/send-mail.js
@@ -3,16 +3,7 @@ const db = require('@arangodb').db;
 const settings = db._collection('foxxy_settings').firstExample();
 const request = require('@arangodb/request');
 const params = module.context.argv[0];
-console.log(`Sent to ${module.context.argv[0].to}`);
-
-request({
-  method: "POST",
-  url: "https://api:"+ settings.mailgun_apikey +"@api.mailgun.net/v3/"+ settings.mailgun_domain +"/messages",
-  form: {
-    from: settings.mailgun_from,
-    to: params.to,
-    subject: "Email confirmation",
-    text: `Hi,
+const email_text = `Hi,
 
 To confirm your email address, please click on the link below :
 https://${settings.domain}/#confirm/${params.uuid}
@@ -21,6 +12,19 @@ If you don't know about this email, please ignore it
 
 Best,
 Foxxy app`
+
+
+console.log(`Sent to ${params.to}`);
+console.log(email_text);
+
+request({
+  method: "POST",
+  url: "https://api:"+ settings.mailgun_apikey +"@api.mailgun.net/v3/"+ settings.mailgun_domain +"/messages",
+  form: {
+    from: settings.mailgun_from,
+    to: params.to,
+    subject: "Email confirmation",
+    text: email_text
   }
 })
 

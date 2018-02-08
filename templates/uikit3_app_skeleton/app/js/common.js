db637af83d92631a5581ece7f242bbc283968efc
Olivier BONNAURE <olivier@solisoft.net>
2018-02-08 14:53:27 +0100

    Fix Tags lists with i18n

--- templates/uikit3_app_skeleton/app/js/common.js
+++ templates/uikit3_app_skeleton/app/js/common.js
@@ -109,6 +109,9 @@ var Common = {
         if(l.t === "tags") {
           html +='<select name="'+l.n+'" style="width:100%" class="select_tag" multiple="multiple">'
           l.d[0].forEach(function(o) {
+            var tags = l.d[0]
+            if(l.tr) tags = _.map(l.d[0], function(t) { return t[window.localStorage.getItem('foxx-locale')]})
+            _.uniq(tags)
             o = l.tr == true ? o[window.localStorage.getItem('foxx-locale')] : [o]
             if(o === undefined || o == "undefined") o = []
             o.forEach(function(v) {

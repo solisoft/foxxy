f1b56a8cba24a078eba1be33808a0e124808da0c
Olivier Bonnaure <olivier@solisoft.net>
2019-02-01 12:07:14 +0100

    Deploy multistage

--- templates/uikit3_app_skeleton/config/deploy.rb
+++ templates/uikit3_app_skeleton/config/deploy.rb
@@ -4,23 +4,13 @@ require 'mina/git'
 require 'YAML'
 
 begin
-  YAML.load(IO.read("config/local_env.yml")).each do |key, value|
+  YAML.load(IO.read('config/local_env.yml'))[@stage].each do |key, value|
     ENV[key.to_s] = value
   end
 rescue
-  puts "config/local_env.yml is missing."
+  puts 'config/local_env.yml is missing.'
   exit
 end
-
-#puts ENV.inspect
-# require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
-
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
 
 set :domain, 'your.domain.net'
 set :deploy_to, '/path/to/deploy/app'

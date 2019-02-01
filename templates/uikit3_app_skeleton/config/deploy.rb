daa43b66de9a82d557ff38f9a380be4cc63ba0ec
Olivier Bonnaure <olivier@solisoft.net>
2019-02-01 15:00:03 +0100

    multidtage deployment

--- templates/uikit3_app_skeleton/config/deploy.rb
+++ templates/uikit3_app_skeleton/config/deploy.rb
@@ -12,11 +12,6 @@ rescue
   exit
 end
 
-set :domain, 'your.domain.net'
-set :deploy_to, '/path/to/deploy/app'
-set :repository, 'git@...'
-set :branch, 'master'
-set :user, 'user'
 
 task :environment do
 end
